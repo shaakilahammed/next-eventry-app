f9f69a64814b04fa1732cc68454da90116479215
shaakilahammed <shaakilahammed@gmail.com>
2024-05-02 11:10:49 +0600

    feat: email send after going

--- actions/event.js
+++ actions/event.js
@@ -1,10 +1,12 @@
 'use server';
+import EmailTemplate from '@/app/components/EmailTemplate';
 import connectMongo from '@/dbConnect/connectMongo';
 import Event from '@/models/Event';
 import { replaceMongoIdInArray, replaceMongoIdInObject } from '@/utils/utils';
 import mongoose from 'mongoose';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { Resend } from 'resend';
 
 export const getAllEvents = async () => {
     try {
@@ -53,10 +55,28 @@ export const updateGoing = async (eventId, auth) => {
 
         event.going_ids.push(new mongoose.Types.ObjectId(auth?.id));
         event.save();
+        await sendEmail(eventId, auth);
 
         revalidatePath('/');
     } catch (error) {
         console.log(error);
     }
     redirect('/');
+};
+
+const sendEmail = async (eventId, user) => {
+    try {
+        // console.log(eventId, user, process.env.RESEND_API_KEY);
+        const event = await getEventbyId(eventId);
+        const resend = new Resend(process.env.RESEND_API_KEY);
+        const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event?.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
+        const sent = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: user?.email,
+            subject: 'Successfully Registered for the event!',
+            react: EmailTemplate({ message }),
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
